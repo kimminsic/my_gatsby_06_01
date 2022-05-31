de2b2ff78acb5c438396579326cfa32a8d0746a0
kimminsic <alstlr135@gmail.com>
2022-05-31 12:37:53 +0900

    테스트페이지

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function testPage() {
+  return (
+    <main>
+      <title>테스트 페이지</title>
+      <h1>테스트 페이지 입니다.</h1>
+    </main>
+  );
+}
+
+export default testPage;
