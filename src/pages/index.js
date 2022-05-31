dc38801097bf934bd435d29eab480b9adfbb97a6
kimminsic <alstlr135@gmail.com>
2022-05-31 12:31:21 +0900

    링크

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { Link } from "gatsby";
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page???</title>
       <h1>Home Page!!!</h1>
+      <Link to="/about">About go go</Link>
     </main>
   );
 };
